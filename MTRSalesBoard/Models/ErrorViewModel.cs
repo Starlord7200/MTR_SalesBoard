e7eea157adba5e4a937f2c46947f68a663aea759
JulianReid541 <reidjulian@outlook.com>
2020-03-13 08:21:07 -0700

    Project Upload

---
+++ MTRSalesBoard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MTRSalesBoard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
