b1e7901291393e939b022444a63dffce4c7f21e3
JulianReid541 <reidjulian@outlook.com>
2020-05-08 11:19:32 -0700

    bug fix for maximum sale entered

--- MTRSalesBoard/Models/SaleEntryViewModel.cs
+++ MTRSalesBoard/Models/SaleEntryViewModel.cs
@@ -9,7 +9,7 @@ namespace MTRSalesBoard.Models
         //If input is more than 1 or 10,000 it'll respond with an error
         #region Properties
         [Required(ErrorMessage = "Amount Required")]
-        [Range(1, 10000, ErrorMessage = "Amount must be greater than 1")]
+        [Range(1, 100000, ErrorMessage = "Amount must be greater than 1")]
         [RegularExpression("^[0-9]{1,10}", ErrorMessage = "Amount must be a number")]
         public decimal SaleAmount { get; set; }
 
--- MTRSalesBoard/Models/UpdateSaleViewModel.cs
+++ MTRSalesBoard/Models/UpdateSaleViewModel.cs
@@ -15,7 +15,7 @@ namespace MTRSalesBoard.Models
         public DateTime Date { get; set; }
 
         [Required(ErrorMessage = "Amount Required")]
-        [Range(1, 10000, ErrorMessage = "Amount must be greater than 1")]
+        [Range(1, 100000, ErrorMessage = "Amount must be greater than 1")]
         [RegularExpression("^[0-9]{1,10}", ErrorMessage = "Amount must be a number")]
         public decimal SaleAmount { get; set; }
 
