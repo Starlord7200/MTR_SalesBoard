587b7986598bfeb0d68bd0a81ab883c5a2a279a8
JulianReid541 <reidjulian@outlook.com>
2021-03-27 20:11:18 -0700

    allows updates up to one million dollars

--- MTRSalesBoard/Models/UpdateSaleViewModel.cs
+++ MTRSalesBoard/Models/UpdateSaleViewModel.cs
@@ -15,7 +15,7 @@ namespace MTRSalesBoard.Models
         public DateTime Date { get; set; }
 
         [Required(ErrorMessage = "Amount Required")]
-        [Range(1, 100000, ErrorMessage = "Amount must be greater than 1")]
+        [Range(1, 1000000, ErrorMessage = "Amount must be greater than 1")]
         [RegularExpression("^[0-9]+(.[0-9]{1,2})?$", ErrorMessage = "Amount must be a number")]
         public decimal SaleAmount { get; set; }
 
