c9acbb4edd52d662a837dbfe4acc2cded4d271b6
JulianReid541 <reidjulian@outlook.com>
2023-05-29 21:06:32 -0700

    Version implemented. Layout shows that accordingly

---
+++ MTRSalesBoard/Version.cs
@@ -0,0 +1,10 @@
+﻿namespace MTRSalesBoard
+{
+    public class Version
+    {
+        // VERSION 1.0
+        //Version one of project complete
+
+        public const string VERSION_NUM = "v1.0.0.0";
+    }
+}
