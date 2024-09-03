20eb9f6108cabf2983d7c7c3dfffc97461c080e6
JulianReid541 <reidjulian@outlook.com>
2024-09-02 20:14:05 -0700

    Added Radzen component library to site

--- SalesBoardBlazor/Program.cs
+++ SalesBoardBlazor/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Radzen;
 using SalesBoardBlazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
